a8a434525c6eef37bb22482e3307d6e156b11f0c
suresh-kumar-coder <135149295+suresh-kumar-coder@users.noreply.github.com>
2024-09-29 21:11:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -56,4 +56,5 @@ if not scheduler.get_job('my_scheduled_job'):
 scheduler.start()
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0",debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
